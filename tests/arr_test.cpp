d623173be8944599caa3b15d40e1e99a498255ac
Viktor Filinkov <x6prlie@gmail.com>
2026-09-17 17:05:46 +0000

    remove filtered_view

--- tests/arr_test.cpp
+++ tests/arr_test.cpp
@@ -1,7 +1,6 @@
 #include <cassert>
 
 #include "base/arr.h"
-#include "base/filter_view.h"
 
 static bool non_zero_occupied(const Arr<int, 5> &arr, Size i) {
 	return arr[i] != 0;
@@ -23,24 +22,6 @@ static void test_arr_standard_iteration() {
 	assert(sum == 10);
 }
 
-static void test_filter_view_iteration() {
-	Arr<int, 5> arr{};
-	arr[1] = 7;
-	arr[3] = 9;
-
-	auto filtered = filter_view(&arr, arr.size(), non_zero_occupied);
-	auto it = filtered.begin();
-	auto end = filtered.end();
-	assert(it != end);
-	assert(*it == 7);
-	++it;
-	assert(it != end);
-	assert(*it == 9);
-	++it;
-	assert(it == end);
-}
-
 int main() {
 	test_arr_standard_iteration();
-	test_filter_view_iteration();
 }
