6ca14521285a02fe6bbbce83765014a1da31d2a4
Viktor Filinkov <x6prlie@gmail.com>
2026-09-21 18:58:21 +0000

    increase switchbutton tap surface size

--- src/ui/components/switch_button.h
+++ src/ui/components/switch_button.h
@@ -2,21 +2,19 @@
 
 #include "button.h"
 
-inline bool switch_button(AppContext *ctx, Clay_ElementId id, bool is_turned_on,
-                          float height = 0.0f) {
+inline bool switch_button(AppContext *ctx, Clay_ElementId id,
+                          bool is_turned_on) {
 	constexpr auto ICON_TOGGLE_OFF = ""_v;
 	constexpr auto ICON_TOGGLE_ON = ""_v;
 
-	if (height <= 0.0f) {
-		height = sizes()->dim.icon_md;
-	}
-
+	const auto height = sizes()->dim.min_touch_target;
 	MobileButtonStyle style{
+		  .min_width = height,
 		  .height = height,
 		  .padding_x = 0,
 		  .padding_y = 0,
 		  .corner_radius = sizes()->radius.sm.topLeft,
-		  .font_size = static_cast<uint16_t>(height),
+		  .font_size = static_cast<uint16_t>(sizes()->dim.icon_md),
 		  .font_id = FontID::ICONS,
 		  .border_width = 0,
 		  .background_pressed = theme()->wrongContainer,
